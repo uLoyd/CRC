ef1c69e8e7dc7eb50bee2248a7112b47d0da3a29
DawidPartyka <dawpar5@wp.pl>
2020-06-28 21:20:46 +0200

    Update crc_02.js

--- CRC/crc_02.js
+++ CRC/crc_02.js
@@ -3,40 +3,55 @@ let crcObj = {
   arr: [],
   fillArr: function(){
     for (let i = 0; i < 256; i++) {
-        let c = i;
         for (let j = 0; j < 8; j++) {
-            c = (c & 1) ? (0xedb88320 ^ (c >>> 1)) : (c >>> 1);
+            this.arr.push((i & 1) ? (0xedb88320 ^ (i >>> 1)) : (i >>> 1));
         }
-        this.arr.push(c);
     }
   },
   calcCRC32: function(str, len){
     if(!this.arr.length) this.fillArr(); //populate polinomials array if empty
-
-    let crc = -1; //Just a start value
+    let crc = -1;                        //Just a start value
 
     for (let i = 0; i < len; i++)
         crc = (crc >>> 8) ^ this.arr[(crc ^ str.charCodeAt(i)) & 0xff];
 
-    crc = crc ^ (-1);
-    return (crc >>> 0).toString(16); //CRC in hexadecimal form
+    return ((crc ^ (-1)) >>> 0).toString(16);     //CRC in hexadecimal form
   }
 }
 //modified from http://crc32.nichabi.com/javascript-function.php
 
-let dataObj = {
-  lineData: [], //Array of objects {line, crc} <--- one string paired with a CRC32 of this strings
+let readFile = (source, callback) => {
+  document.getElementById(source).onchange = (evt) => {
+    const file = evt.target.files[0];
 
+    if(file){
+      const read = new FileReader();
+
+      read.onload = (content) => {
+        callback(content.target.result, file.type);
+      }
+      read.onerror = () => {
+        console.log(reader.error);
+        callback(false);
+      };
+      read.readAsText(file);
+    }
+    else{
+      callback(false);
+    }
+  }
+}
+
+let dataObj = {
+  lineData: [],                  //Array of objects {line, crc} <--- one string paired with a CRC32 of this strings
   getData: function (str){
     str = str.match(/.{1,32}/g); // <-- Block size = 32 ---- divides whole file into array of 32 char long stringsOpt
 
     //Fill obj and calculate CRC32 for indivdual strings
     str.forEach(function(entry){
       dataObj.lineData.push({line: entry, crc: crcObj.calcCRC32(entry, entry.length)});
-      //console.log(crcObj.calcCRC32(entry, entry.length));
-      //console.log(entry);
     });
-    //console.log(this.lineData);
+
     this.writeFile();
   },
   writeFile: function(){
@@ -44,7 +59,7 @@ let dataObj = {
     let content = "";
 
     //----------Get file extension---------
-    let extensionData = {
+    const extensionData = {
       ext: null,
       type: function(){
         return (ext == "txt" ? "text/plain" : "application/json");
@@ -81,86 +96,60 @@ let dataObj = {
     //----------Prepare data to make file--------------
 
     //--Create the text file as a Blob:
-  	let blob = new Blob([content],{type: extensionData.type()});
+  	const blob = new Blob([content],{type: extensionData.type()});
 
   	//--Download the file:
-  	download(blob,"crc." + extensionData.ext); //file name and extension
+  	download(blob, `crc.${extensionData.ext}`); //file name and extension
 
   	function download(blob,name) {
-  		let url = URL.createObjectURL(blob),
-  		div = document.createElement("div"),
-  		anch = document.createElement("a");
+      //--Download the file:
+      let anch = document.createElement("a");
+      anch.href = URL.createObjectURL(blob);
+      anch.download = name;
 
-  		document.body.appendChild(div);
-  		div.appendChild(anch);
-
-  		anch.innerHTML = "&nbsp;";
-  		div.style.width = "0";
-  		div.style.height = "0";
-  		anch.href = url;
-  		anch.download = name;
-
-  		var ev = new MouseEvent("click",{});
-  		anch.dispatchEvent(ev);
-  		document.body.removeChild(div);
+      //----Trigger download and clean up:
+      const ev = new MouseEvent("click",{});
+      anch.dispatchEvent(ev);
   	}
   }
-
 }
 
 //----------------DOM elements for displaying checked data--------------------
 let addToDom = {
   it: 0,
   addElem: function(x){
-      let parent = $(x.target);
-      let cl = (x.cl != null ? ' class="' + x.cl + '"': " ");
-      let extra = (x.extra != null ? x.extra : "");
-      let text = (x.text != null ? x.text : "");
-      let id = (x.id != null ? ' id = "' + x.id + '"' : " ");
+      const parent = $(x.target),
+      cl = (x.cl != null ?  `class="${x.cl}"`: ""),
+      extra = (x.extra != null ? x.extra : ""),
+      text = (x.text != null ? x.text : ""),
+      id = (x.id != null ? `id="${x.id}"` : "");
 
-      parent.append("<" + x.sel + id + cl +" "+ extra +">"+ text +"</"+ x.sel +">");
+      parent.append(`<${x.sel} ${id} ${cl} ${extra}>${text}</${x.sel}>`);
   },
   getInfo: function(x){
     if(this.addLog(x.crcCheck, x.crcOrg, x.line))
       this.addFile(x.line);
-    else {
+    else
       this.addFile("\n!INVALID CRC IN GIVEN LINE!\n");
-    }
   },
   addLog: function(crcC, crcO, line){
-    let stat = (crcC === crcO ? "OK" : "INVALID");
-    //...add to log
-
-    let row = {
-      sel: 'p',
-      cl: 'logRow',
-      target: '#log',
-      id: 'lr' + this.it
+    function DOMobject(sel, tar, cl, txt) {
+      this.sel = sel;
+      this.target = tar;
+      this.cl = cl;
+      this.text = txt;
     }
+    const stat = (crcC === crcO ? "OK" : "INVALID");
+
+    const row = new DOMobject('p', '#log', 'logRow', null);
+    row.id = `lr${this.it}`;
     addToDom.addElem(row);
 
-    let add = {
-      sel: 'p',
-      target: '#' + row.id,
-      cl: 'logLine',
-      text: 'Checked line: ' + line
-    }
+    const add      = new DOMobject('p', `#${row.id}`, 'logLine', `Checked line: ${line}`);
+    const crcCheck = new DOMobject('p', add.target, 'logCrc', `Given CRC: ${crcO.toUpperCase()}\nCalculated CRC: ${crcC.toUpperCase()}`);
+    const st       = new DOMobject('p', add.target, stat, `Status: ${stat}`);
 
-    let crcCheck = {
-      sel: 'p',
-      target: add.target,
-      cl: 'logCrc',
-      text: "Given CRC: " + crcO.toUpperCase() + "\t Calculated CRC: " + crcC.toUpperCase()
-    }
-
-    let st = {
-      sel: 'p',
-      target: add.target,
-      cl: stat,
-      text: "Status: " + stat
-    }
-
-    $("#" + row.id).ready(function(){
+    $(`#${row.id}`).ready(function(){
       addToDom.addElem(add);
       addToDom.addElem(crcCheck);
       addToDom.addElem(st);
@@ -178,79 +167,53 @@ let addToDom = {
 
 $(document).ready(function(){
   //File upload to calculate CRC
-  document.getElementById('inputfile').onchange = function (evt) {
-    var f = evt.target.files[0];
+  readFile('inputfile', (data) => {
+    if(data)
+      dataObj.getData(data.substr(0, data.length).replace(/\r?\n|\r/g,"\\n"));
 
-    //Sanity check
-    if (f){
-      var r = new FileReader();
-
-      r.onload = function(e) {
-        var contents = e.target.result;
-
-        let ctrlPoint = 0;
-        //dataObj.getData(contents.substr(0, contents.length).replace(/\r?\n|\r/g, ' '));
-        dataObj.getData(contents.substr(0, contents.length).replace(/\r?\n|\r/g,"\\n"));
-      }
-
-      r.readAsText(f);
-
-      } else {
-        alert("No file uploaded");
-      }
-  }
+    else
+      console.log('No file uploaded');
+  });
 
   //CRC check
-  document.getElementById('checkfile').onchange = function (evt) {
-    var f = evt.target.files[0];
+  readFile('checkfile', (recData, type) => {
+    if(recData){
+      //Check if file extension is correct
+      if(type == "text/plain"){
+        let txt = recData.split('\n').map(function(ln){
+          return ln.split('\t');
+        });
 
-    //Sanity check
-    if(f){
-      var r = new FileReader();
+        //Loop through text file
+        txt.forEach(function(entry){
+          //...check CRC
+          const crcChk = crcObj.calcCRC32(entry[1], entry[1].length);
+          addToDom.getInfo({crcCheck: crcChk, crcOrg: entry[0], line: entry[1]})
+        });
+      }
+      else if (type == "application/json") {
+        const data = JSON.parse(recData);
 
-      r.onload = function(e) {
-        var contents = e.target.result;
-
-        //Check if file extension is correct
-        if(f.type == "text/plain"){
-          let txt = contents.split('\n').map(function(ln){
-            return ln.split('\t');
-          });
-
-          //Loop through text file
-          txt.forEach(function(entry){
-            //...check CRC
-            let crcChk = crcObj.calcCRC32(entry[1], entry[1].length);
-            addToDom.getInfo({crcCheck: crcChk, crcOrg: entry[0], line: entry[1]})
-
-          });
-        }
-        else if (f.type == "application/json") {
-          let data = JSON.parse(contents);
-
-          //Loop through JSON
-          for(let i = 0; i < data.length; i++){
-            //...check CRC
-            if(data[i].crc === crcObj.calcCRC32(data[i].line, data[i].line.length)){
-              //...display line & CRC ok status
-              console.log("Line: " + data[i].line + "\tStatus: OK");
-            }
-            else{
-              //...display line & CRC fail status
-              console.log("Line: " + data[i].line + "\tStatus: ERROR\nCRC: " + data[i].crc + "\tcalculated: " + crcObj.calcCRC32(data[i].line, data[i].line.length));
-            }
+        //Loop through JSON
+        for(let i = 0; i < data.length; i++){
+          //...check CRC
+          if(data[i].crc === crcObj.calcCRC32(data[i].line, data[i].line.length)){
+            //...display line & CRC ok status
+            console.log(`Line: ${data[i].line}\tStatus: OK`);
+          }
+          else{
+            //...display line & CRC fail status
+            console.log(`Line: ${data[i].line}\tStatus: ERROR\nCRC: ${data[i].crc}\tcalculated: ${crcObj.calcCRC32(data[i].line, data[i].line.length)}`);
           }
         }
-        //Unrecognized file extension
-        else
-          alert("Sorry, this file is invalid, allowed extensions are: .txt .json");
       }
+      //Unrecognized file extension
+      else
+        alert("Sorry, this file is invalid, allowed extensions are: .txt .json");
+    }
 
-      r.readAsText(f);
-
-      } else {
-        alert("No file uploaded");
-      }
-  }
+    else
+      console.log('No file uploaded');
+  });
 
 });
